313d17dfb5469bfab4311ed1b0ed9876c55027c3
shuichifujita <chaashuuu@gmail.com>
2016-03-06 00:36:55 +0900

    add gitignore and new dir of tutorial

---
+++ tutorial_01/Counter.java
@@ -0,0 +1,15 @@
+class Counter {
+  int num;
+
+  void init() {
+    num = 0;
+  }
+
+  void count() {
+    num = num + 1;
+  }
+
+  void printNum() {
+    System.out.println(num);
+  }
+}
---
+++ tutorial_01/Say.java
@@ -0,0 +1,9 @@
+class Say {
+  void hello() {
+    System.out.println("Hello!");
+  }
+
+  void goodBye() {
+    System.out.println("Good Bye!");
+  }
+}
