e8bf6d7da4beae9491d73a4afd2dbc5c69392ed0
shuichifujita <chaashuuu@gmail.com>
2016-03-06 23:58:34 +0900

    change directory and add class printing default encoding
    change directry

---
+++ tutorial_01/Hello/Sample.java
@@ -0,0 +1,12 @@
+/**
+* created: 2016.2.6
+* class name: Sample
+* WTD: println
+*/
+class Sample {
+  public static void main(String arg[]){
+    // 画面出力
+    System.out.println("Hello world!");
+    System.out.println("Good bye world!");
+  }
+}
---
+++ tutorial_01/PrintEncoding/PrintEncoding.java
@@ -0,0 +1,8 @@
+import java.util.Properties;
+import java.util.Enumeration;
+
+class PrintEncoding {
+  public static void main(String[] args) {
+    System.out.println(System.getProperty("file.encoding"));
+  }
+}
